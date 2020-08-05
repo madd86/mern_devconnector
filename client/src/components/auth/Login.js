b2e1f0e8a508d8580eaf09d8527f7113c3f0fec3
Gabriel Leon de Matos <gabriel@Gabriels-iMac.local>
2020-08-05 18:47:16 -0400

    Added

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 
- const Login = () => {
+const Register = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const { email, password } = formData
+
+    const onChange = (e) =>
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        console.log('success')
+    }
+
     return (
-        <div>
-            
-        </div>
+        <Fragment>
+            <h1 className='large text-primary'>Sign In</h1>
+            <p className='lead'>
+                <i className='fas fa-user'></i> Sign into your account
+            </p>
+            <form className='form' onSubmit={(e) => onSubmit(e)}>
+                <div className='form-group'>
+                    <input
+                        type='email'
+                        placeholder='Email Address'
+                        name='email'
+                        value={email}
+                        onChange={(e) => onChange(e)}
+                        required
+                    />
+                </div>
+                <div className='form-group'>
+                    <input
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        value={password}
+                        onChange={(e) => onChange(e)}
+                        minLength='6'
+                    />
+                </div>
+                <input
+                    type='submit'
+                    className='btn btn-primary'
+                    value='Login'
+                />
+            </form>
+            <p className='my-1'>
+                Don't have an Account? <Link to='/register'>Sign Up</Link>
+            </p>
+        </Fragment>
     )
 }
 
-export default Login
+export default Register
