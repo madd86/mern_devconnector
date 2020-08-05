624a077b97e8aa50439285b85f84931109048a3b
Gabriel Leon de Matos <gabriel@Gabriels-iMac.local>
2020-08-05 19:42:16 -0400

    Added

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,12 @@
 import React, { Fragment, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { setAlert } from '../../actions/alert'
+import { register } from '../../actions/auth'
+import PropTypes from 'prop-types'
 
 
-const Register = () => {
+const Register = ({ setAlert, register }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -18,9 +22,9 @@ const Register = () => {
     const onSubmit = async (e) => {
         e.preventDefault()
         if (password !== password2) {
-            console.log('Passwords do not match')
+            setAlert('Passwords do not match', 'danger')
         } else {
-            console.log('success')
+            register({ name, email, password })
         }
     }
 
@@ -38,7 +42,6 @@ const Register = () => {
                         name='name'
                         value={name}
                         onChange={(e) => onChange(e)}
-                        required
                     />
                 </div>
                 <div className='form-group'>
@@ -48,7 +51,6 @@ const Register = () => {
                         name='email'
                         value={email}
                         onChange={(e) => onChange(e)}
-                        required
                     />
                     <small className='form-text'>
                         This site uses Gravatar so if you want a profile image,
@@ -62,7 +64,6 @@ const Register = () => {
                         name='password'
                         value={password}
                         onChange={(e) => onChange(e)}
-                        minLength='6'
                     />
                 </div>
                 <div className='form-group'>
@@ -72,7 +73,6 @@ const Register = () => {
                         name='password2'
                         value={password2}
                         onChange={(e) => onChange(e)}
-                        minLength='6'
                     />
                 </div>
                 <input
@@ -88,4 +88,9 @@ const Register = () => {
     )
 }
 
-export default Register
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired
+}
+
+export default connect(null, { setAlert, register })(Register)
