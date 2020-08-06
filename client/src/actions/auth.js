7a5b743521900828df778f2af225652ab81eb782
Gabriel Leon de Matos <gabriel@Gabriels-iMac.local>
2020-08-05 22:17:00 -0400

    Logout Button

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_SUCCESS,
-    LOGIN_FAIL
+    LOGIN_FAIL,
+    LOGOUT
 } from './types'
 import setAuthToken from '../utils/setAuthToken'
 
@@ -46,7 +47,7 @@ export const register = ({ name, email, password }) => async (dispatch) => {
             type: REGISTER_SUCCESS,
             payload: res.data,
         })
-        
+
         dispatch(loadUser())
     } catch (err) {
         const errors = err.response.data.errors
@@ -90,4 +91,9 @@ export const login = (email, password) => async (dispatch) => {
             type: LOGIN_FAIL,
         })
     }
+}
+
+// Logout / Clear Profiles
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT })
 }
