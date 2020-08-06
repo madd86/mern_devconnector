25a1a1145fe5c49be6f6fd602c6372f0a94e5c58
Gabriel Leon de Mattos <gabriel@Gabriels-iMac.local>
2020-08-06 07:51:54 -0400

    Load profile text, part 1

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,27 +1,34 @@
-import { GET_PROFILE, PROFILE_ERROR } from "../actions/types"
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types'
 
 const initialState = {
     profile: null,
     profiles: [],
     repos: [],
     loading: true,
-    error: {}
+    error: {},
 }
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
     const { type, payload } = action
 
-    switch(type) {
+    switch (type) {
         case GET_PROFILE:
             return {
                 ...state,
                 profile: payload,
-                loading: false
+                loading: false,
             }
         case PROFILE_ERROR:
             return {
                 ...state,
                 error: payload,
+                loading: false,
+            }
+        case CLEAR_PROFILE:
+            return {
+                ...state,
+                profile: null,
+                repos: [],
                 loading: false
             }
         default:
