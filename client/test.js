73b8ed4022d072f59d09e1f4f9561b1fca1503d9
Gabriel Leon de Mattos <gabriel@Gabriels-iMac.local>
2020-08-07 21:29:54 -0400

    Show and Delete Edcation / Experience

---
+++ client/test.js
@@ -0,0 +1,18 @@
+const output = {
+    "b": {
+      "d": "null",
+      "e": [
+        "apple",
+        "prop",
+        {
+          "today": "Interview"
+        }
+      ]
+    },
+    "f": {
+      "g": "goals",
+      "h": "I am full of life"
+    },
+    "i": 1
+  }
+  
