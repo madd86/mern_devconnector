e3515a811379d16fabfabd09e5a28d60f922324a
Gabriel Leon de Mattos <gabriel@Gabriels-iMac.local>
2020-08-08 22:54:25 -0400

    Added profile page and visualizations

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import CreateProfile from './components/profile-forms/CreateProfile'
 import EditProfile from './components/profile-forms/EditProfile'
 import AddExperience from './components/profile-forms/AddExperience'
 import AddEducation from './components/profile-forms/AddEducation'
+import Profiles from './components/profiles/Profiles'
 
 if (localStorage.token) setAuthToken(localStorage.token)
 
@@ -39,6 +40,7 @@ const App = () => {
                                 component={Register}
                             />
                             <Route exact path='/login' component={Login} />
+                            <PrivateRoute exact path='/profiles' component={Profiles} />
                             <PrivateRoute exact path='/dashboard' component={Dashboard} />
                             <PrivateRoute exact path='/create-profile' component={CreateProfile} />
                             <PrivateRoute exact path='/edit-profile' component={EditProfile} />
